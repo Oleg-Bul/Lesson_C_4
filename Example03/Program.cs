ef300a68492d6920e47d801240971dda8e6452cf
Oleg_bul <kokseg63@gmail.com>
2022-10-01 14:13:46 +0400

    final

--- Example03/Program.cs
+++ Example03/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Factorial (int n)
+{
+    if(n ==1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
